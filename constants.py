738b7742ccb204e6cd4d12e691c3c2fcde820d44
TanveerSM <130005493+TanveerSM@users.noreply.github.com>
2025-04-29 11:17:00 -0400

    Update constants.py
    changed the url to the real id link instead of the test url

--- constants.py
+++ constants.py
@@ -4,7 +4,7 @@ phone_no :int = "123456789"
 email :str = "email@gmail.com"
 licence_no :str = "X11112222233333"
 # url that the browser will start from
-url : str = r"https://telegov.njportal.com/njmvc/AppointmentWizard/11/"
+url : str = r"https://telegov.njportal.com/njmvc/AppointmentWizard/12/"
 #acceptable location, case sensitive
 # specify which dmv location you are willing to go to
 acceptable_locations: str = ["Eatontown", "Edison", "Freehold", "Rahway", "South Plainfield"]
