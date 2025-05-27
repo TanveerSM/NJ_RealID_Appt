0ef674c7b431efd00ac21ad1aafb8d767ea8ce62
TanveerSM <130005493+TanveerSM@users.noreply.github.com>
2025-05-27 10:31:38 -0400

    corrected type hints

--- constants.py
+++ constants.py
@@ -1,14 +1,14 @@
 first_name :str = "Johnny"
 last_name :str = "Doe"
-phone_no :int = "123456789"
+phone_no :int = 123456789
 email :str = "email@gmail.com"
 licence_no :str = "X11112222233333"
 # url that the browser will start from
 url :str = r"https://telegov.njportal.com/njmvc/AppointmentWizard/12/"
 #acceptable location, case sensitive
 # specify which dmv location you are willing to go to
-acceptable_locations :str = ["Eatontown", "Edison", "Freehold", "Rahway", "South Plainfield"]
+acceptable_locations :list[str] = ["Eatontown", "Edison", "Freehold", "Rahway", "South Plainfield"]
 acceptable_year :int = 2025
-acceptable_month :list = [4,5,6]
-acceptable_weekday :list = [0,1,2,3,4,5,6] #Monday 0, Sunday 6
-acceptable_hour :list = [8,9,10,11,12,13,14,15,16] #hour in 24H format
+acceptable_month :list[int] = [4,5,6]
+acceptable_weekday :list[int] = [0,1,2,3,4,5,6] #Monday 0, Sunday 6
+acceptable_hour :list[int] = [8,9,10,11,12,13,14,15,16] #hour in 24H format
