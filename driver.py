2af014218612418ae340aad47742a259091600d2
tsm933 <TanveerSM@github.com>
2025-05-28 12:25:11 -0400

    added gitignore and corrected syntax errors in functions.py

--- driver.py
+++ driver.py
@@ -6,11 +6,11 @@ def browser_config():
     #Browser config
     # Specifies the location of the firefox executable
     options = Options()
-    binary = "C:\Program Files\Mozilla Firefox\Firefox.exe"
+    binary = "/usr/bin/firefox-esr"
     options.binary_location = binary
 
     # specifies the location of geckodriver
-    geckodriver = "E:\Downloads\geckodriver-v0.36.0-win64\geckodriver.exe"
+    geckodriver = "/home/XXXX/Selenium Drivers/geckodriver"
     service = Service(executable_path=geckodriver)
     # configures and launches the firefox browser
     driver = webdriver.Firefox(options=options, service=service)
